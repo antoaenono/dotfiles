646b2b0bf025b3e078fbf7884419687811c82bd4
Anton Bilbaeno <antoaenono@pm.me>
2025-09-12 10:47:38 -0700

    brew bundle for package install

--- scripts/run_apply.sh
+++ scripts/run_apply.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-source ~/.config/zsh/zshrc
-echo "zsh sourced!"
+# this no work cuz it is run somewhere else (not the current terminal where I ran `chezmoi apply`)
+# source ~/.config/zsh/zshrc
