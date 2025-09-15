7980432a3c579682daaf13e4898b1117df35c345
Anton Bilbaeno <antoaenono@pm.me>
2025-09-14 20:24:24 -0700

    doom cfg

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dotfiles
+Need I say more?
--- scripts/run_after_apply.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-# this no work cuz it is run somewhere else (not the current terminal where I ran `chezmoi apply`)
-# source ~/.config/zsh/zshrc
